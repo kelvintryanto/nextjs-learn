85e9170e0597d91cef136c22a0acebb9d02618cd
kelvintryanto <kelvin.tryanto@gmail.com>
2024-05-30 08:25:19 +0700

    Improving Accessibility 2

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -72,7 +72,11 @@ export async function createInvoice(prevState: State, formData: FormData) {
   redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, prevState:State, formData: FormData) {
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData,
+) {
   const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
@@ -104,7 +108,7 @@ export async function updateInvoice(id: string, prevState:State, formData: FormD
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
+  // throw new Error('Failed to Delete Invoice');
 
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
